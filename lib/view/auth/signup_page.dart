e899150bfdf44531817778aaae7abcafd008283e
MaulanaArya30 <maulanaaryaw@gmail.com>
2023-02-19 13:56:27 +0700

    fixed login sign up

--- lib/view/auth/signup_page.dart
+++ lib/view/auth/signup_page.dart
@@ -1,5 +1,4 @@
-import 'dart:js';
-
+//import 'dart:js';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/gestures.dart';
